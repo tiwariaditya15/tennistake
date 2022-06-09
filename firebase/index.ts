c64a23aec58dcf6af5758042628aa219f761a531
Aditya Tiwari <tiwariaditya15@hotmail.com>
2022-06-10 02:14:47 +0530

    style: prosemirror global styles

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: process.env["apiKey"],
-  authDomain: process.env["authDomain"],
-  projectId: process.env["projectId"],
-  storageBucket: process.env["storageBucket"],
-  messagingSenderId: process.env["messagingSenderId"],
-  appId: process.env["appId"],
-  measurementId: process.env["measurementId"],
+  apiKey: "AIzaSyAU8fEYKkNM4rft9YXyADfyT5jkFapyDvc",
+  authDomain: "tennistake-a5b6f.firebaseapp.com",
+  projectId: "tennistake-a5b6f",
+  storageBucket: "tennistake-a5b6f.appspot.com",
+  messagingSenderId: "14307210760",
+  appId: "1:14307210760:web:c65f0849434c0b69b7c3bd",
+  measurementId: "G-QFDBMKV05E",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const db = getFirestore(app);
